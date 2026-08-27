26c584e7253bc0ea0aaf15ed3350d67eb2cb02ee
Vorp_al <146890741+VorpalRoblox@users.noreply.github.com>
2026-08-27 01:15:15 +0100

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
 const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-const base = isGitHubPages && repository ? `/${repository}` : '/';
+const base = isGitHubPages && repository ? `/${repository}/` : '/';
 
 export default defineConfig({
   output: 'static',
